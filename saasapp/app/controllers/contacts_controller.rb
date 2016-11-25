58a5ffed6c1a672efe710dbb52c058e657fbe338
Anil Shrestha <jesnlishrestha04@gmail.com>
2016-11-25 07:32:32 +0000

    Add Trigger to contacts mailer

--- saasapp/app/controllers/contacts_controller.rb
+++ saasapp/app/controllers/contacts_controller.rb
@@ -6,6 +6,10 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new(contact_params)
         if @contact.save
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = = params[:contact][:comments]
+            ContactMailer.contact_email(name, email, body).deliver
             flash[:success] = "Message sent."
             redirect_to new_contact_path
         else
@@ -17,7 +21,7 @@ class ContactsController < ApplicationController
     private
         def contact_params
             params.require(:contact).permit(:name, :email, :comments)
-        end
+        end 
          
 end
  
